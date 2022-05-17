49f526339546fd7df6f2ee3c6ea1c5e21370524e
Sandeep Kumar <sandeepkumar.adimulam@jobiak.com>
2022-05-17 11:55:02 +0530

    Adding Project

---
+++ Day9 WrapperClasses/src/ai/jobiak/wrappers/TestWrappers.java
@@ -0,0 +1,22 @@
+package ai.jobiak.wrappers;
+
+public class TestWrappers {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Number number = new Integer("10");
+		System.out.println(number);
+		number = new Integer(new String("10"));
+		System.out.println(number);
+		
+		int i=10;
+		String str = "hello";
+		Double radius = new Double("25.75");
+		
+		
+		
+	}
+
+}
